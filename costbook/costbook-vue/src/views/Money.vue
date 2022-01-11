7a934779b1416e05d12d9aeff6fa40fcbb9a03e8
胖虎 <329661967@qq.com>
2022-01-12 01:25:29 +0800

    添加loading 修改header

---
+++ costbook/costbook-vue/src/views/Money.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+   Money
+   <Nav/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Money",
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ costbook/costbook-vue/src/views/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>stats
+  <Nav/>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "Statistics"
+}
+</script>
+
+<style scoped>
+
+</style>
